44f4b56cce9c0ba8a7697ba101e05a7142849322
Icosahunter <anzelknight@gmail.com>
2023-05-06 19:54:01 -0500

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    const markdownIt = require('markdown-it')
+    const markdownItAttrs = require('markdown-it-attrs')
+    const markdownItOptions = {
+      html: true,
+      breaks: true,
+      linkify: true
+    }
+    const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+    eleventyConfig.addPassthroughCopy('halfmoonui');
+    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('custom-variables.css');
+    eleventyConfig.setNunjucksEnvironmentOptions({
+      throwOnUndefined: true,
+      autoescape: false, // warning: don’t do this!
+    });
+    eleventyConfig.setLibrary('md', markdownLib);
+};
